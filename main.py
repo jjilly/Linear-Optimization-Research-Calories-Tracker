5222cc6deef94aab06c7b39ae28d10c9a9e6da2c
Leslie Z <Leslie.zha.95@gmail.com>
2018-11-23 15:37:52 -0800

    presentation and script paramters added (need to update readme)

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import argparse
 from gurobipy import *
 
 # Nutritional food information base on:
-# USDA Dietary Guidelines(2005)
-# http://www.health.gov/DietaryGuidelines/dga2005/
+# USDA Dietary Guidelines
 
 categories, minNutrition, maxNutrition = multidict({
   'calories': [1800, 2200],
@@ -15,13 +14,13 @@ categories, minNutrition, maxNutrition = multidict({
 
 foods, costs = multidict({
   'meatball': 2.49,
-  'chicken':   2.89,
-  'meat pie':   1.50,
-  'potatoes':     1.89,
-  'pasta':  2.09,
-  'pizza':     1.99,
-  'salad':     2.49,
-  'milk':      0.89,
+  'chicken': 2.89,
+  'meat pie': 1.50,
+  'potatoes': 1.89,
+  'pasta': 2.09,
+  'pizza': 1.99,
+  'salad': 2.49,
+  'milk': 0.89,
   'ice cream': 1.59})
 
 foods, costs_multiplier = multidict({
@@ -47,41 +46,41 @@ def function_costs(time=0):
 # Nutrition values for the foods
 nutritionValues = {
   ('meatball', 'calories'): 410,
-  ('meatball', 'protein'):  24,
-  ('meatball', 'fat'):      26,
-  ('meatball', 'carbohydrates'):   40,
+  ('meatball', 'protein'): 24,
+  ('meatball', 'fat'): 26,
+  ('meatball', 'carbohydrates'): 40,
   ('chicken',   'calories'): 420,
-  ('chicken',   'protein'):  32,
-  ('chicken',   'fat'):      10,
-  ('chicken',   'carbohydrates'):   0,
+  ('chicken',   'protein'): 32,
+  ('chicken',   'fat'): 10,
+  ('chicken',   'carbohydrates'): 0,
   ('meat pie',   'calories'): 560,
-  ('meat pie',   'protein'):  20,
-  ('meat pie',   'fat'):      32,
-  ('meat pie',   'carbohydrates'):   45,
+  ('meat pie',   'protein'): 20,
+  ('meat pie',   'fat'): 32,
+  ('meat pie',   'carbohydrates'): 45,
   ('potatoes',     'calories'): 380,
-  ('potatoes',     'protein'):  4,
-  ('potatoes',     'fat'):      19,
-  ('potatoes',     'carbohydrates'):   39,
+  ('potatoes',     'protein'): 4,
+  ('potatoes',     'fat'): 9,
+  ('potatoes',     'carbohydrates'): 39,
   ('pasta',  'calories'): 320,
-  ('pasta',  'protein'):  12,
-  ('pasta',  'fat'):      10,
-  ('pasta',  'carbohydrates'):   75,
+  ('pasta',  'protein'): 12,
+  ('pasta',  'fat'): 10,
+  ('pasta',  'carbohydrates'): 75,
   ('pizza',     'calories'): 320,
-  ('pizza',     'protein'):  15,
-  ('pizza',     'fat'):      12,
-  ('pizza',     'carbohydrates'):   37,
+  ('pizza',     'protein'): 15,
+  ('pizza',     'fat'): 12,
+  ('pizza',     'carbohydrates'): 37,
   ('salad',     'calories'): 320,
-  ('salad',     'protein'):  31,
-  ('salad',     'fat'):      12,
-  ('salad',     'carbohydrates'):   25,
+  ('salad',     'protein'): 31,
+  ('salad',     'fat'): 12,
+  ('salad',     'carbohydrates'): 25,
   ('milk',      'calories'): 100,
-  ('milk',      'protein'):  8,
-  ('milk',      'fat'):      2.5,
-  ('milk',      'carbohydrates'):   13,
+  ('milk',      'protein'): 8,
+  ('milk',      'fat'): 2.5,
+  ('milk',      'carbohydrates'): 13,
   ('ice cream', 'calories'): 330,
-  ('ice cream', 'protein'):  8,
-  ('ice cream', 'fat'):      10,
-  ('ice cream', 'carbohydrates'):   32}
+  ('ice cream', 'protein'): 8,
+  ('ice cream', 'fat'): 10,
+  ('ice cream', 'carbohydrates'): 32}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--penalty', required=False)
@@ -106,7 +105,6 @@ if penalty == "none":
   m.setObjective(buy.prod(costs), GRB.MINIMIZE)
 elif penalty == "quadratic":
   penalized_objective = QuadExpr()
-
   for food, price in zip(buy.select(), costs.select()):
     penalized_objective.add(price * food + (.5) * food * food)
   # 2: To set to food amount penalized objective
@@ -123,11 +121,11 @@ m.addConstrs(
 def printSolution():
     if m.status == GRB.Status.OPTIMAL:
         print('\nOptimal Solution: %g' % m.objVal)
-        print('\nTO BUY (in grams):')
+        print('\nTO EAT (servings):')
         buyx = m.getAttr('x', buy)
         cost = 0
         for f in foods:
-            if buy[f].x > 0.01:
+            if buy[f].x > 0.1:
                 print('%s %g' % (f, buyx[f]))
                 cost += buyx[f] * costs[f]
 
